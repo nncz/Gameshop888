0142713c41e8760cf312e751549ab7fcbb4bf199
nncz <65011212125@msu.ac.th>
2025-10-27 03:36:29 +0700

    fix mysql ssl for railway on render

--- db.ts
+++ db.ts
@@ -1,13 +1,18 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 dotenv.config();
-const pool = mysql.createPool({
+
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true // ✅ จำเป็น ต้องเปิด SSL สำหรับ Railway
+  }
 });
-
-export default pool;
+export default db;  // ✅ เพิ่มบรรทัดนี้
